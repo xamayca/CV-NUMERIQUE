19d64b20e6d525bbc558e5c56fbcdc8c56466d51
xamayca <Christopher.hariouat@gmail.com>
2023-10-26 13:42:46 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Get the id for the navigation menu animation //
+const navLinksHtml = document.getElementById('navigation-links');
+const menuOpenHtml = document.getElementById('menu-open');
+const menuCloseHtml = document.getElementById('menu-close');
+
+// On click, use its classes on the menu button and links //
+menuOpenHtml.addEventListener('click', () => {
+    navLinksHtml.classList.toggle('d-initial');
+    menuOpenHtml.classList.toggle('d-none');
+    menuCloseHtml.classList.toggle('d-none');
+});
+
+menuCloseHtml.addEventListener('click', () => {
+    navLinksHtml.classList.toggle('d-initial');
+    menuOpenHtml.classList.toggle('d-none');
+    menuCloseHtml.classList.toggle('d-none');
+});
+
