560bfcefe5eed9c7ba2b646806eb843124d3c874
xamayca <Christopher.hariouat@gmail.com>
2023-10-30 14:46:00 +0100

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,21 @@ const navLinksHtml = document.getElementById('navigation-links');
 const menuOpenHtml = document.getElementById('menu-open');
 const menuCloseHtml = document.getElementById('menu-close');
 
-// On click, use its classes on the menu button and links //
+// On click add class to navLinks/menuOpen/menuClose & disable scroll //
 menuOpenHtml.addEventListener('click', () => {
     navLinksHtml.classList.toggle('d-initial');
     menuOpenHtml.classList.toggle('d-none');
     menuCloseHtml.classList.toggle('d-none');
+
+    document.body.style.overflow = 'hidden';
 });
 
+// On click remove class to navLinks/menuOpen/menuClose & enable scroll //
 menuCloseHtml.addEventListener('click', () => {
     navLinksHtml.classList.toggle('d-initial');
     menuOpenHtml.classList.toggle('d-none');
     menuCloseHtml.classList.toggle('d-none');
+
+    document.body.style.overflow = 'auto';
 });
 
