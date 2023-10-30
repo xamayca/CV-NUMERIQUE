a6cb6673d3743ea462a1731a2ad1a710fa2758c1
xamayca <christopher.hariouat@gmail.com>
2023-10-30 19:04:13 +0100

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const navLinksHtml = document.getElementById('navigation-links');
 const menuOpenHtml = document.getElementById('menu-open');
 const menuCloseHtml = document.getElementById('menu-close');
 
-// On click add class to navLinks/menuOpen/menuClose & disable scroll on menu mobile //
+// On click add class from css to navLinks/menuOpen/menuClose & disable scroll on menu mobile //
 menuOpenHtml.addEventListener('click', () => {
     navLinksHtml.classList.toggle('d-initial');
     menuOpenHtml.classList.toggle('d-none');
@@ -12,12 +12,12 @@ menuOpenHtml.addEventListener('click', () => {
     document.body.classList.toggle('overflow-h');
 });
 
-// On click remove class to navLinks/menuOpen/menuClose & enable scroll on menu mobile //
+// On click remove class from css to navLinks/menuOpen/menuClose & enable scroll on menu mobile //
 menuCloseHtml.addEventListener('click', () => {
     navLinksHtml.classList.toggle('d-initial');
     menuOpenHtml.classList.toggle('d-none');
     menuCloseHtml.classList.toggle('d-none');
 
-    document.body.classList.toggle('overflow-v');
+    document.body.classList.toggle('overflow-auto');
 });
 
